1af37e6912c8958dc3fd4a656fad59bf79a49ed5
dumgum84 <99304115+dumgum84@users.noreply.github.com>
2026-06-03 14:31:46 -0400

    Update __init__.py

--- ytp_dl/__init__.py
+++ ytp_dl/__init__.py
@@ -1,5 +1,5 @@
 """ytp-dl: YouTube video downloader with Mullvad VPN integration"""
 
-__version__ = "2026.5.25"
+__version__ = "2026.5.28"
 __author__  = "dumgum82"
 __email__   = "dumgum42@gmail.com"
